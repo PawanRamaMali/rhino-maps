9ea8241cd16802d50c47af6dffadf32001c13a2c
Pawan Rama Mali <prm@outlook.in>
2023-04-10 22:51:04 +0530

    add dependencies

--- dependencies.R
+++ dependencies.R
@@ -1,2 +1,9 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(rhino)
+library(reactable) # for reactable table output
+library(echarts4r) # for echarts4r charts
+library(dplyr) # for dplyr data manipulation
+library(tidyr) # for tidyr data manipulation
+library(htmlwidgets) # for htmlwidgets
+
+#renv::install(c('rhino', 'reactable', 'echarts4r', 'dplyr', 'tidyr', 'htmlwidgets'))
