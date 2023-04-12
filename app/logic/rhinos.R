e6f44f8400e6337b77c3bcdac38c46ced004beb5
Pawan Rama Mali <prm@outlook.in>
2023-04-12 23:59:17 +0530

    v0.3
    added js to remove commas from charts

--- app/logic/rhinos.R
+++ app/logic/rhinos.R
@@ -1,6 +1,7 @@
 box::use(
     rhino,
     reactable[reactable],
+    htmlwidgets[JS],
     tidyr,
     dplyr,
     echarts4r,
@@ -25,6 +26,6 @@ chart <- function(data){
     dplyr$group_by(Species) |>
     echarts4r$e_chart(Year) |>
     echarts4r$e_line(Population) |>
-    echarts4r$e_x_axis(Year) |>
+    echarts4r$e_x_axis(Year, formatter = JS("App.formatYear")) |>
     echarts4r$e_tooltip()
 }
