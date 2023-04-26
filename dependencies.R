2e60d212bc6506d5605d5753c08a51820c219ff3
Pawan Rama Mali <prm@outlook.in>
2023-04-26 23:47:26 +0530

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -5,5 +5,8 @@ library(echarts4r) # for echarts4r charts
 library(dplyr) # for dplyr data manipulation
 library(tidyr) # for tidyr data manipulation
 library(htmlwidgets) # for htmlwidgets
-
+library(leaflet)
+library(fst)
+library(sf)
+library(data.table)
 #renv::install(c('rhino', 'reactable', 'echarts4r', 'dplyr', 'tidyr', 'htmlwidgets'))
