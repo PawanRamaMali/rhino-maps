320dd8f0dfb59786fc9ea32dddd326ac12f2b2bf
Pawan Rama Mali <prm@outlook.in>
2023-04-26 23:48:32 +0530

    Update main.R

--- app/main.R
+++ app/main.R
@@ -1,32 +1,37 @@
 box::use(
+  fst[read_fst],
+  sf[st_read],
   shiny[bootstrapPage, div, moduleServer, NS, reactive, titlePanel],
 )
 box::use(
   app/logic/rhinos,
-  app/view[chart, table],
+  app/view[chart, map],
 )
-
-grid <- function(...) div(class = 'grid', ...)
-card <- function(...) div(class = "card", ...)
 
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
   bootstrapPage(
-    titlePanel("Rhino population over time"),
-    grid(
-      table$ui(ns("table")),
-      chart$ui(ns("chart"))
+    titlePanel("World Map"),
+    map$ui(ns("map")),
+    chart$ui(ns("chart"))
     )
-  )
+  
 }
 
 #' @export
 server <- function(id) {
+  # moduleServer(id, function(input, output, session) {
+  #   data <- reactive(rhinos$fetch_data())
+  #   table$server("table", data)
+  #   chart$server("chart", data)
+  # })
+  
   moduleServer(id, function(input, output, session) {
-    data <- reactive(rhinos$fetch_data())
-    table$server("table", data)
-    chart$server("chart", data)
+    print("loading dataset")
+    medal_data <- read_fst("app/data/medal_data.fst", as.data.table = TRUE)
+    map_data <- st_read(dsn = "app/data/map_data.shp", quiet = TRUE)
+    map$server("map", map_data, medal_data)
   })
 }
