9056219f8b7e40c72bf0ebf7881c697cec12c378
Pawan Rama Mali <prm@outlook.in>
2023-04-30 21:07:33 +0530

    Update leaflet_script.R

--- leaflet_script.R
+++ leaflet_script.R
@@ -2,5 +2,17 @@ library(leaflet)
 
 m <- leaflet() |>  
   addTiles() |> 
-  setView( lng = -149.49, lat = 64.2008, zoom = 4)
+  setView( lng = -149.49, lat = 64.2008, zoom = 4) |> 
+  addProviderTiles(providers$Thunderforest.Pioneer)
+m
+
+
+m <- leaflet() |>  
+  addProviderTiles(providers$Thunderforest) |> 
+  setView( lng = -149.49, lat = 64.2008, zoom = 4) 
+m
+
+m <- leaflet() |>  
+  addProviderTiles(providers$Esri.NatGeoWorldMap) |> 
+  setView( lng = -149.49, lat = 64.2008, zoom = 4) 
 m
