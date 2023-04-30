7575b40034e578c173525c7967eb875e6cbd8e64
Pawan Rama Mali <prm@outlook.in>
2023-04-30 21:18:42 +0530

    Update leaflet_script.R

--- leaflet_script.R
+++ leaflet_script.R
@@ -8,7 +8,7 @@ m
 
 
 m <- leaflet() |>  
-  addProviderTiles(providers$Thunderforest) |> 
+  addProviderTiles(providers$Stamen.TerrainBackground) |> 
   setView( lng = -149.49, lat = 64.2008, zoom = 4) 
 m
 
@@ -16,3 +16,14 @@ m <- leaflet() |>
   addProviderTiles(providers$Esri.NatGeoWorldMap) |> 
   setView( lng = -149.49, lat = 64.2008, zoom = 4) 
 m
+
+
+readRenviron(".Renviron")
+apikey <- Sys.getenv("THUNDERFOREST_API_KEY")
+variant <-  "Landscape"
+leaflet() %>%
+  addTiles(
+    urlTemplate = "https://{s}.tile.thunderforest.com/{variant}/{z}/{x}/{y}.png?apikey={apikey}",
+    attribution = "&copy; <a href='http://www.thunderforest.com/'>Thunderforest</a>,  &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>",
+    options = tileOptions(variant='landscape', apikey = apikey)
+  )
